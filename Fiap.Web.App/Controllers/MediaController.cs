7d8a533645c1f8ce78e3003f3fef2b1cee3543fa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-05 22:47:12 -0300

    Adiciona calculo de media

---
+++ Fiap.Web.App/Controllers/MediaController.cs
@@ -0,0 +1,39 @@
+﻿using Fiap.Web.App.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.App.Controllers
+{
+    public class MediaController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Media media)
+        {
+            float Media1 = media.Media1;
+            float Media2 = media.Media2;
+            float MediaTotal = media.CalculaMedia();
+            bool Aprovacao = media.ValidacaoAprovacao(MediaTotal);
+
+            //Console.WriteLine(Media1);
+            //Console.WriteLine(Media2);
+            //Console.WriteLine(MediaTotal);
+            if (Aprovacao)
+            {
+                Console.WriteLine("Parabéns, você foi aprovado com: " + MediaTotal);
+                ViewBag.conteudo = "Parabéns, você foi aprovado com: " + MediaTotal;
+            }
+            else
+            {
+                Console.WriteLine("Sinto muito, você foi reprovado com: " + MediaTotal);
+                ViewBag.conteudo = "Sinto muito, você foi reprovado com: " + MediaTotal;
+            }
+            return View();
+        }
+    }
+}
---
+++ Fiap.Web.App/Model/Media.cs
@@ -0,0 +1,26 @@
+﻿namespace Fiap.Web.App.Model
+{
+    public class Media
+    {
+
+        public float Media1 { get; set; }
+        public float Media2 { get; set; }
+        public float MediaTotal { get; set; }
+
+        public float CalculaMedia() 
+        {
+            MediaTotal = (Media1 + Media2) / 2;
+            return MediaTotal;
+        }
+
+        public bool ValidacaoAprovacao(float MediaTotal)
+        {
+            if (MediaTotal > 6) {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
